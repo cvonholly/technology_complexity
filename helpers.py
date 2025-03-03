e7f119ef5bd02483912581f9ca12d1937c3ad009
Carl von Holly <cvonholly@ikmail.com>
2025-03-03 17:00:38 +0100

    run ML approach for Wind (takes 48 minutes for 1.6 MB run locally, very long)

--- helpers.py
+++ helpers.py
@@ -17,3 +17,43 @@ def response_key(endpoint:str) -> str:
         return leaf + 'es'
     else:
         return leaf + 's'
+    
+
+
+# for claims
+import pandas as pd
+import re
+import math as mt
+
+def get_claims(df: pd.DataFrame, id: int) -> pd.DataFrame:
+    claims_text = df.loc[(df['Publn_Nr'] == id) & (df['Type'].str.match('Claim')), 'Text']  # get claim text
+    return claims_text
+
+def concantenate_claims(claims: pd.Series) -> str:
+    return ' '.join(claims)
+
+def checkRoman(token):
+    re_pattern = '[mdcxvi]+[a-z]'
+    if re.fullmatch(re_pattern, token):
+        return True
+    return False
+
+def update_total_counts(counts_total: dict, tokens: list) -> dict:
+    for token, count in tokens.items():
+        if token in counts_total:
+            counts_total[token] += count
+        else:
+            counts_total[token] = count
+    return counts_total
+
+def get_entropy(counts: dict) -> float:
+    """
+    Calculate the entropy of a dictionary of token counts
+    """
+    entropy = 0
+    total = sum(counts.values())
+    for token, count in counts.items():
+        if count > 0:
+            p = count / total
+            entropy = p * mt.log2(1/p)
+    return entropy
